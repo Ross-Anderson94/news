80450343538a60455e8f8e25156943f1e5a605b3
Ross Anderson <2268991A@student.gla.ac.uk>
2021-04-20 10:49:31 +0100

    first commit

---
+++ news/rango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	context_dict = {'boldmessage': 'test'}
+
+	return render(request, 'rango/index.html', context=context_dict)
+
